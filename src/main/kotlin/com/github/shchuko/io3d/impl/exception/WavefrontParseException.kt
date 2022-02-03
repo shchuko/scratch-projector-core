682b47d8cb979c28bc128711db923076390bc911
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2022-02-03 22:45:35 +0300

    [io3d] Remove unused ctors of WavefrontParseException

--- src/main/kotlin/com/github/shchuko/io3d/impl/exception/WavefrontParseException.kt
+++ src/main/kotlin/com/github/shchuko/io3d/impl/exception/WavefrontParseException.kt
@@ -3,10 +3,8 @@ package com.github.shchuko.io3d.impl.exception
 import com.github.shchuko.io3d.exception.ParseException
 
 class WavefrontParseException : ParseException {
-    constructor() : super()
     constructor(message: String, line: Int) : this("Line $line: $message")
     constructor(message: String, cause: Throwable?, line: Int) : this("Line $line: $message", cause)
     constructor(message: String) : super(message)
     constructor(message: String, cause: Throwable?) : super(message, cause)
-    constructor(cause: Throwable?) : super(cause)
 }
