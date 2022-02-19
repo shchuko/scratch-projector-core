effd27b2e4a4734ead90a2a88599fdd9dccfb39e
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2022-02-19 17:03:03 +0300

    [io2d] Writer WIP

--- src/main/kotlin/com/github/shchuko/io2d/Writer.kt
+++ src/main/kotlin/com/github/shchuko/io2d/Writer.kt
@@ -1,5 +1,5 @@
 package com.github.shchuko.io2d
 
 interface Writer {
-    fun write(scene: Scene2D, canvasProperties: Scene2D.CanvasProperties)
+    fun write(scene: Scene2D, canvasProperties: CanvasProcessor.CanvasProperties)
 }
--- src/main/kotlin/com/github/shchuko/io2d/impl/MinimalSvgWriter.kt
+++ src/main/kotlin/com/github/shchuko/io2d/impl/MinimalSvgWriter.kt
@@ -1,10 +1,11 @@
 package com.github.shchuko.io2d.impl
 
+import com.github.shchuko.io2d.CanvasProcessor
 import com.github.shchuko.io2d.Scene2D
 import com.github.shchuko.io2d.Writer
 
-class MinimalSvgWriter : Writer {
-    override fun write(scene: Scene2D, canvasProperties: Scene2D.CanvasProperties) {
+open class MinimalSvgWriter : Writer {
+    override fun write(scene: Scene2D, canvasProperties: CanvasProcessor.CanvasProperties) {
         TODO("Not yet implemented")
     }
 }
